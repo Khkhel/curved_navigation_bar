aba81b68aced937325eea91f68408223a698abb4
Khkhel <142094574+Khkhel@users.noreply.github.com>
2023-08-13 10:24:30 +0100

    Update curved_navigation_bar.dart

--- lib/curved_navigation_bar.dart
+++ lib/curved_navigation_bar.dart
@@ -113,7 +113,7 @@ class CurvedNavigationBarState extends State<CurvedNavigationBar>
               child: Transform.translate(
                 offset: Offset(
                   0,
-                  -(1 - _buttonHide) * 80,
+                  -(1 - _buttonHide) * 40,
                 ),
                 child: Material(
                   color: widget.buttonBackgroundColor ?? widget.color,
